98adc81a57adc1c24a8a8b5839e596061f2024a6
Alexander Stone <aloecend@gmail.com>
2018-04-13 09:43:39 -0700

    removed comments and changed document.ready to an arrow function.

--- alex-ben/scripts/articleView.js
+++ alex-ben/scripts/articleView.js
@@ -79,7 +79,7 @@ articleView.setTeasers = function() {
   });
 };
 
-$(document).ready(function() {
+$(document).ready( () => {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
